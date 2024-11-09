14dab4eeeafab3b33d4226307fb12517deeb6f52
Nikita <nikitamelnikov502@gmail.com>
2024-11-09 13:10:01 +0500

    Cleaned bot and bot_router code

--- bot.py
+++ bot.py
@@ -4,170 +4,104 @@ import logging
 import os
 
 import aio_pika
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.filters import Command, CommandStart
+from aiogram import Bot, Dispatcher, Router
 from dotenv import load_dotenv
 from datetime import datetime
-from aiogram.types import CallbackQuery
 
-from bot_router import router
-from db.database import db
-from db.database import init_db, close_db
-from keyboards.keyboard import keyboard
+from bot_router import setup_routes
+from db.database import db, init_db, close_db
 
-load_dotenv()
+class BotApp:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.api_token = os.environ.get("BOT_TOKEN")
+        self.fastapi_url = os.environ.get("FASTAPI_URL")
+        self.rabbitmq_url = os.environ.get("RABBITMQ_URL")
 
-FASTAPI_URL = os.environ.get("FASTAPI_URL")
-URL = os.environ.get("URL")
-logger = logging.getLogger(__name__)
-RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
+        self.bot = Bot(token=self.api_token)
+        self.dp = Dispatcher()
+        self.router = Router()
+        setup_routes(self.router)
+        self.dp.include_router(self.router)
 
-bot = Bot(token=os.environ.get("BOT_TOKEN"))
-dp = Dispatcher()
-
-@dp.message(CommandStart())
-async def start(msg: types.Message):
-    async with db.transaction():
-        result = await db.first("SELECT * FROM users WHERE user_id=$1", msg.from_user.id)
-        if not result:
-            await db.status("INSERT INTO users (user_id, user_limit, status, chat_id) VALUES ($1, $2, 'not_authorized', $3)", msg.from_user.id, 0, msg.chat.id)
-            await msg.answer("Добро пожаловать!", reply_markup=keyboard)
-        else:
-            await msg.answer("Добро пожаловать обратно!", reply_markup=keyboard)
-
-
-@dp.message(Command("help"))
-async def help(msg: types.Message):
-    await msg.answer("""
-    Добро пожаловать в бота для отслеживания новых постов в группах ВК!
-    Список доступных команд:
-        /start - начать работу с ботом
-        /help - помощь
-        /authorize - авторизоваться через ВК
-        /add_group - добавить группу
-        /remove_group - удалить группу
-        /updates_on - подключить уведомления 
-        /updates_off - отключить уведомления
-""", reply_markup=keyboard)
-
-
-@dp.message(Command("authorize"))
-async def open_link(msg: types.Message):
-    builder = InlineKeyboardBuilder()
-    builder.row(types.InlineKeyboardButton(
-        text="Авторизоваться", url=f"{URL}?user_id={msg.from_user.id}", callback_data="auth")
-    )
-
-    await msg.answer("Авторизуйтесь через ВК:", reply_markup=builder.as_markup())
-
-
-@dp.callback_query(lambda query: query.data == "auth" and query.data.startswith("auth"))
-async def open_github_link(query: CallbackQuery):
-    async with db.transaction():
-        db.status("UPDATE users SET status = 'in progress' WHERE user_id = $2", query.from_user.id)
-
-
-@dp.message(Command("get_chat_id"))
-async def get_chat_id(msg: types.Message):
-    await msg.answer(str(msg.chat.id))
-
-async def send_messages(bot: Bot, text: str, group_id: int):
-    """
-    Отправка сообщений всем авторизованным пользователям.
-    """
-    try:
-        chat_ids = []
-        
-        async with db.transaction():
-            user_ids = await db.all("SELECT user_id FROM users_groups_rel WHERE group_id = $1", group_id)
-
-            chat_ids = []
-            for (user_id,) in user_ids:
-                chat_id_row = await db.first("SELECT chat_id FROM users WHERE user_id = $1", user_id)
-                if chat_id_row:
-                    chat_id = chat_id_row[0]
-                    chat_ids.append(chat_id)
-
-        send_tasks = [
-            bot.send_message(chat_id=chat_id, text=text)
-            for chat_id in chat_ids
-        ]
-
-        results = await asyncio.gather(*send_tasks, return_exceptions=True)
-
-        for chat_id, result in zip(chat_ids, results):
-            if isinstance(result, Exception):
-                logger.error(f"Не удалось отправить сообщение пользователю {chat_id}: {result}")
-            else:
-                logger.info(f"Сообщение успешно отправлено пользователю {chat_id}.")
-    except Exception as e:
-        logger.exception(f"Ошибка при отправке сообщений: {e}")
-
-async def process_message(message: aio_pika.IncomingMessage, bot: Bot):
-    """
-    Обработка одного сообщения из очереди RabbitMQ.
-    """
-    async with message.process():
+    async def start_polling(self):
+        await init_db()
         try:
-            body = message.body.decode()
-            data = json.loads(body)
-            text = f"""
-                {datetime.fromisoformat(data['post_date'])}
-                {data['group_name']}
-                {data['post_text']}
-                {data['post_link']}
-            """.strip()
-
-            logger.info(f"Обработка сообщения: {data['post_id']}")
-
-            await send_messages(bot, text, data['group_id'])
-
+            consumer_task = asyncio.create_task(self.consume_messages())
+            await self.dp.start_polling(self.bot)
+            await consumer_task
         except Exception as e:
-            logger.exception(f"Ошибка при обработке сообщения: {e}")
+            self.logger.exception("Ошибка во время работы основного процесса")
+        finally:
+            await close_db()
 
-
-async def consume_messages(bot: Bot):
-    """
-    Подключение к RabbitMQ и потребление сообщений.
-    """
-    while True:
+    async def send_messages(self, text: str, group_id: int):
+        """
+        Отправка сообщений всем авторизованным пользователям.
+        """
         try:
-            connection = await aio_pika.connect_robust(RABBITMQ_URL)
-            async with connection:
-                channel = await connection.channel()
-                await channel.set_qos(prefetch_count=10)
-                queue = await channel.declare_queue("posts_queue", durable=True)
-                logger.info(f"Подключено к очереди posts_queue в RabbitMQ.")
+            async with db.transaction():
+                user_ids = await db.all("SELECT user_id FROM users_groups_rel WHERE group_id = $1", group_id)
+                chat_ids = [
+                    (await db.first("SELECT chat_id FROM users WHERE user_id = $1", user_id))[0]
+                    for (user_id,) in user_ids if user_id
+                ]
 
-                async with queue.iterator() as queue_iter:
-                    async for message in queue_iter:
-                        await process_message(message, bot)
+            send_tasks = [self.bot.send_message(chat_id=chat_id, text=text) for chat_id in chat_ids]
+            results = await asyncio.gather(*send_tasks, return_exceptions=True)
 
-        except Exception as e:
-            logger.exception(f"Ошибка подключения или потребления сообщений: {e}")
-            logger.info("Повторная попытка подключения через 5 секунд...")
-            await asyncio.sleep(5)
+            for chat_id, result in zip(chat_ids, results):
+                if isinstance(result, Exception):
+                    self.logger.error(f"Не удалось отправить сообщение пользователю {chat_id}: {result}")
+                else:
+                    self.logger.info(f"Сообщение успешно отправлено пользователю {chat_id}.")
+        except Exception:
+            self.logger.exception("Ошибка при отправке сообщений")
+
+    async def process_message(self, message: aio_pika.IncomingMessage):
+        """
+        Обработка одного сообщения из очереди RabbitMQ.
+        """
+        async with message.process():
+            try:
+                data = json.loads(message.body.decode())
+                text = f"""
+                    {datetime.fromisoformat(data['post_date'])}
+                    {data['group_name']}
+                    {data['post_text']}
+                    {data['post_link']}
+                """.strip()
+                await self.send_messages(text, data['group_id'])
+            except Exception:
+                self.logger.exception("Ошибка при обработке сообщения")
+
+    async def consume_messages(self):
+        """
+        Подключение к RabbitMQ и потребление сообщений.
+        """
+        while True:
+            try:
+                connection = await aio_pika.connect_robust(self.rabbitmq_url)
+                async with connection:
+                    channel = await connection.channel()
+                    await channel.set_qos(prefetch_count=10)
+                    queue = await channel.declare_queue("posts_queue", durable=True)
+                    self.logger.info("Подключено к очереди posts_queue в RabbitMQ.")
+
+                    async with queue.iterator() as queue_iter:
+                        async for message in queue_iter:
+                            await self.process_message(message)
+            except Exception:
+                self.logger.exception("Ошибка подключения или потребления сообщений, повторная попытка через 5 секунд")
+                await asyncio.sleep(5)
+
 
 async def main():
-    try:
-        await init_db()
+    load_dotenv()
+    logging.basicConfig(level=logging.INFO)
+    app = BotApp()
+    await app.start_polling()
 
-        logging.basicConfig(level=logging.INFO)
-
-        consumer_task = asyncio.create_task(consume_messages(bot))
-        
-        dp.include_router(router)
-
-        await dp.start_polling(bot)
-        
-        await consumer_task
-
-    except Exception as e:
-        print(e)
-    finally:
-        await close_db()
-        
 
 if __name__ == "__main__":
     asyncio.run(main())
--- bot_router.py
+++ bot_router.py
@@ -1,7 +1,11 @@
+# bot_router.py
+import os
 from aiogram import F, types, Router
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import CallbackQuery
 
 from utils.functions import (
     check_link, 
@@ -11,107 +15,126 @@ from utils.functions import (
     get_group_names
 )
 from db.database import db
-
-from keyboards.inline import inline_keyboard
-
-router = Router()
+from keyboards.keyboard import keyboard
 
 class AddingGroup(StatesGroup):
     add_group = State()
 
+def setup_routes(router: Router):
+    @router.message(Command("help"))
+    async def help(msg: types.Message):
+        await msg.answer("""
+        Добро пожаловать в бота для отслеживания новых постов в группах ВК!
+        Список доступных команд:
+            /start - начать работу с ботом
+            /help - помощь
+            /authorize - авторизоваться через ВК
+            /add_group - добавить группу
+            /remove_group - удалить группу
+            /updates_on - подключить уведомления 
+            /updates_off - отключить уведомления
+        """, reply_markup=keyboard)
 
-@router.message(Command("authorize"))
-async def auth(msg: types.Message):
-    async with db.transaction():
-        db.status("UPDATE users SET status = 'in progress' WHERE user_id = $2", msg.from_user.id) 
-    await msg.answer("Нажмите кнопку ниже, чтобы перейти на страницу аутентификации:", reply_markup=inline_keyboard)
+    @router.message(Command("start"))
+    async def start(msg: types.Message):
+        async with db.transaction():
+            result = await db.first("SELECT * FROM users WHERE user_id=$1", msg.from_user.id)
+            if not result:
+                await db.status("INSERT INTO users (user_id, user_limit, status, chat_id) VALUES ($1, $2, 'not_authorized', $3)", msg.from_user.id, 0, msg.chat.id)
+                await msg.answer("Добро пожаловать!", reply_markup=keyboard)
+            else:
+                await msg.answer("Добро пожаловать обратно!", reply_markup=keyboard)
 
-
-@router.message(Command("updates_on"))
-async def turn_off_updates(msg: types.Message):
-    async with db.transaction():
-        await db.status("UPDATE users SET updates = TRUE WHERE user_id = $1", msg.from_user.id)
-    await msg.answer("Вы успешно включили уведомления о новых постах!")
-
-
-@router.message(Command("updates_off"))
-async def turn_off_updates(msg: types.Message):
-    async with db.transaction():
-        await db.status("UPDATE users SET updates = FALSE WHERE user_id = $1", msg.from_user.id)
-    await msg.answer("Вы успешно отключили уведомления о новых постах!")
-
-
-@router.message(Command("add_group"))
-async def set_add_group(msg: types.Message, state: FSMContext):
-    await state.set_state(AddingGroup.add_group)
-    await msg.answer("Введите ссылку на группу: ")
-
-
-@router.message(AddingGroup.add_group, Command("stop"))
-async def stop_getting_link(msg: types.Message, state: FSMContext):
-    await state.clear()
-    await msg.answer("Вы отменили ввод!") 
-
-
-@router.message(AddingGroup.add_group, F.text)
-async def accept_link(msg: types.Message, state: FSMContext):
-    user_id = msg.from_user.id
-    link = msg.text.strip()
-
-    correct_link = await check_link(link, msg.from_user.id)
-    limit = await check_limit(user_id)
-
-    if not correct_link and not limit:
-        await msg.answer(
-            "Вы ввели некорректную ссылку, а также исчерпали свой лимит на добавление групп! "
-            "Приобретите подписку!"
+    @router.message(Command("authorize"))
+    async def auth(msg: types.Message):
+        builder = InlineKeyboardBuilder()
+        builder.row(types.InlineKeyboardButton(
+            text="Авторизоваться", url=f"{os.environ.get('URL')}?user_id={msg.from_user.id}", callback_data="auth")
         )
-        return
+        await msg.answer("Авторизуйтесь через ВК:", reply_markup=builder.as_markup())
 
-    if not correct_link:
-        await msg.answer("Вы ввели некорректную ссылку!")
-        return
+    @router.callback_query(lambda query: query.data == "auth" and query.data.startswith("auth"))
+    async def open_github_link(query: CallbackQuery):
+        async with db.transaction():
+            db.status("UPDATE users SET status = 'in progress' WHERE user_id = $2", query.from_user.id)
 
-    if not limit:
-        await msg.answer(
-            "Вы достигли лимита! Больше групп добавить не получится. "
-            "Оформите подписку, чтобы увеличить количество групп."
-        )
+    @router.message(Command("updates_on"))
+    async def turn_off_updates(msg: types.Message):
+        async with db.transaction():
+            await db.status("UPDATE users SET updates = TRUE WHERE user_id = $1", msg.from_user.id)
+        await msg.answer("Вы успешно включили уведомления о новых постах!")
+
+    @router.message(Command("updates_off"))
+    async def turn_off_updates(msg: types.Message):
+        async with db.transaction():
+            await db.status("UPDATE users SET updates = FALSE WHERE user_id = $1", msg.from_user.id)
+        await msg.answer("Вы успешно отключили уведомления о новых постах!")
+
+    @router.message(Command("add_group"))
+    async def set_add_group(msg: types.Message, state: FSMContext):
+        await state.set_state(AddingGroup.add_group)
+        await msg.answer("Введите ссылку на группу: ")
+
+    @router.message(AddingGroup.add_group, Command("stop"))
+    async def stop_getting_link(msg: types.Message, state: FSMContext):
         await state.clear()
-        return
+        await msg.answer("Вы отменили ввод!") 
 
-    result = await add_group(user_id, link)
-    if result:
-        await msg.answer(
-            "Вы успешно добавили группу! Если хотите продолжить, введите ещё одну ссылку, или нажмите /stop."
-        )
-    else:
-        await msg.answer("Произошла неизвестная ошибка. Обратитесь к администратору или попробуйте позднее.")
+    @router.message(AddingGroup.add_group, F.text)
+    async def accept_link(msg: types.Message, state: FSMContext):
+        user_id = msg.from_user.id
+        link = msg.text.strip()
 
+        correct_link = await check_link(link, msg.from_user.id)
+        limit = await check_limit(user_id)
 
-@router.message(AddingGroup.add_group, ~F.text)
-async def got_not_link(msg: types.Message):
-    await msg.answer("Введите, ссылку, пожалуйста. Если хотите прекратить,  нажмите /stop") 
+        if not correct_link and not limit:
+            await msg.answer(
+                "Вы ввели некорректную ссылку, а также исчерпали свой лимит на добавление групп! "
+                "Приобретите подписку!"
+            )
+            return
 
+        if not correct_link:
+            await msg.answer("Вы ввели некорректную ссылку!")
+            return
 
-@router.message(Command("remove_group"))
-async def delete_group(msg: types.Message):
-    await msg.answer("Введите ссылку на группу, которую хотите удалить: ")
+        if not limit:
+            await msg.answer(
+                "Вы достигли лимита! Больше групп добавить не получится. "
+                "Оформите подписку, чтобы увеличить количество групп."
+            )
+            await state.clear()
+            return
 
-    result = await remove_group(msg.from_user.id, msg.text.strip())
+        result = await add_group(user_id, link)
+        if result:
+            await msg.answer(
+                "Вы успешно добавили группу! Если хотите продолжить, введите ещё одну ссылку, или нажмите /stop."
+            )
+        else:
+            await msg.answer("Произошла неизвестная ошибка. Обратитесь к администратору или попробуйте позднее.")
 
-    if result:
-        await msg.answer("Вы успешно удалили группу!")
+    @router.message(AddingGroup.add_group, ~F.text)
+    async def got_not_link(msg: types.Message):
+        await msg.answer("Введите, ссылку, пожалуйста. Если хотите прекратить,  нажмите /stop") 
 
-    else:   
-        await msg.answer("Произошла неизвестная ошибка. Обратитесь к администратору или попробуйте позднее.")
+    @router.message(Command("remove_group"))
+    async def delete_group(msg: types.Message):
+        await msg.answer("Введите ссылку на группу, которую хотите удалить: ")
 
+        result = await remove_group(msg.from_user.id, msg.text.strip())
 
-@router.message(Command("get_group_list"))
-async def get_groups(msg: types.Message):
-    groups = await get_group_names()
+        if result:
+            await msg.answer("Вы успешно удалили группу!")
+        else:   
+            await msg.answer("Произошла неизвестная ошибка. Обратитесь к администратору или попробуйте позднее.")
 
-    if not groups:
-        await msg.answer("Произошла ошибка при получении списка групп!")
-    else:
-        await msg.answer("Ваши группы:\n" + "\n".join(groups))
+    @router.message(Command("get_group_list"))
+    async def get_groups(msg: types.Message):
+        groups = await get_group_names()
+
+        if not groups:
+            await msg.answer("Произошла ошибка при получении списка групп!")
+        else:
+            await msg.answer("Ваши группы:\n" + "\n".join(groups))
